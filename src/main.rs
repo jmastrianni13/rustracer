e07e8e62db5d73334bfc7dec2e1cba6b7b194ae5
Joe Mastrianni <jmastrianni13@gmail.com>
2025-09-08 21:55:27 -0400

    added world

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,22 @@
+use std::rc::Rc;
 mod color;
+mod hittable;
+mod hittable_list;
 mod ray;
+mod rtweekend;
+mod sphere;
 mod vec3;
 
 fn main() {
     render_image();
 }
 
-fn hit_sphere(center: ray::Point3, radius: f64, r: &ray::Ray) -> f64 {
-    let oc = center - r.orig.clone();
-    let a = r.dir.length_squared();
-    let h = vec3::get_dot_prod(&r.dir, &oc);
-    let c = oc.length_squared() - radius * radius;
-    let discriminant = h * h - a * c;
-
-    if discriminant < 0.0 {
-        return -1.0;
-    } else {
-        return (h - f64::sqrt(discriminant)) / a;
+fn ray_color(r: &ray::Ray, world: &dyn hittable::Hittable) -> color::Color {
+    let mut rec = hittable::HitRecord::default();
+    if world.hit(r, 0.0, rtweekend::infinity, &mut rec) {
+        return (rec.normal + color::Color::new(1.0, 1.0, 1.0)) * 0.5;
     }
-}
 
-fn ray_color(r: &ray::Ray) -> color::Color {
-    let t = hit_sphere(ray::Point3::new(0.0, 0.0, -1.0), 0.5, r);
-
-    if t > 0.0 {
-        let n = (r.at(t) - vec3::Vec3::new(0.0, 0.0, -1.0)).unit_vector();
-        return color::Color::new(n.x + 1.0, n.y + 1.0, n.z + 1.0) * 0.5;
-    }
     let unit_direction = r.dir.unit_vector();
     let a = 0.5 * (unit_direction.y + 1.0);
     return color::Color::new(1.0, 1.0, 1.0) * (1.0 - a) + color::Color::new(0.5, 0.7, 1.0) * a;
@@ -36,7 +26,22 @@ fn render_image() {
     // image
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400;
-    let image_height = (image_width as f64 / aspect_ratio).max(1.0) as i32;
+    let mut image_height = (image_width as f64 / aspect_ratio).max(1.0) as i32;
+    if image_height < 1 {
+        image_height = 1;
+    }
+
+    // world
+    let mut world = hittable_list::HittableList::new();
+
+    world.add(Rc::new(sphere::Sphere::new(
+        ray::Point3::new(0.0, 0.0, -1.0),
+        0.5,
+    )));
+    world.add(Rc::new(sphere::Sphere::new(
+        ray::Point3::new(0.0, -100.5, -1.0),
+        100.0,
+    )));
 
     // camera
     let focal_length = 1.0;
@@ -76,7 +81,7 @@ fn render_image() {
                 + (pixel_delta_v.clone() * h_idx as f64);
             ray_direction = pixel_center - camera_center.clone();
             r = ray::Ray::new(camera_center.clone(), ray_direction);
-            pixel_color = ray_color(&r);
+            pixel_color = ray_color(&r, &world);
             color::write_color(pixel_color);
         }
     }
