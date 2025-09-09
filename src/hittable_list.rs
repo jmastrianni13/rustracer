31d39107e8bb496b06ed9e4cd2c65c46b7e9fd18
Joe Mastrianni <jmastrianni13@gmail.com>
2025-09-08 21:54:00 -0400

    added sphere and hittable list

---
+++ src/hittable_list.rs
@@ -0,0 +1,47 @@
+use crate::hittable;
+use crate::ray;
+use std::rc::Rc;
+
+pub struct HittableList {
+    objects: Vec<Rc<dyn hittable::Hittable>>,
+}
+
+impl HittableList {
+    pub fn new() -> Self {
+        return HittableList {
+            objects: Vec::new(),
+        };
+    }
+
+    pub fn clear(&mut self) {
+        self.objects = Vec::new();
+    }
+
+    pub fn add(&mut self, object: Rc<dyn hittable::Hittable>) {
+        self.objects.push(object);
+    }
+}
+
+impl hittable::Hittable for HittableList {
+    fn hit(
+        &self,
+        r: &ray::Ray,
+        ray_tmin: f64,
+        ray_tmax: f64,
+        rec: &mut hittable::HitRecord,
+    ) -> bool {
+        let mut temp_rec: hittable::HitRecord = rec.clone();
+        let mut hit_anything = false;
+        let mut closest_so_far = ray_tmax.clone();
+
+        for object in &self.objects {
+            if object.hit(r, ray_tmin, closest_so_far, &mut temp_rec) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                *rec = temp_rec.clone()
+            }
+        }
+
+        return hit_anything;
+    }
+}
---
+++ src/sphere.rs
@@ -0,0 +1,51 @@
+use crate::hittable;
+use crate::ray;
+use crate::vec3;
+
+pub struct Sphere {
+    center: ray::Point3,
+    radius: f64,
+}
+
+impl Sphere {
+    pub fn new(center: ray::Point3, radius: f64) -> Self {
+        return Self {
+            center,
+            radius: radius.max(0.0),
+        };
+    }
+}
+
+impl hittable::Hittable for Sphere {
+    fn hit(
+        &self,
+        r: &ray::Ray,
+        ray_tmin: f64,
+        ray_tmax: f64,
+        rec: &mut hittable::HitRecord,
+    ) -> bool {
+        let oc = self.center.clone() - r.orig.clone();
+        let a = r.dir.length_squared();
+        let h = vec3::get_dot_prod(&r.dir, &oc);
+        let c = oc.length_squared() - self.radius * self.radius;
+        let discriminant = h * h - a * c;
+
+        if discriminant < 0.0 {
+            return false;
+        }
+
+        let sqrtd = f64::sqrt(discriminant);
+        let root = (h - sqrtd) / a;
+
+        if (root <= ray_tmin) || (ray_tmax <= root) {
+            return false;
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        let outward_normal = (rec.p.clone() - self.center.clone()) / self.radius;
+        rec.set_face_normal(r, &outward_normal);
+
+        return true;
+    }
+}
